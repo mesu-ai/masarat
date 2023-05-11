90d2951a3a8d91e279c7c5c86239d7780c962bca
mesu <mesu.s.ss@gmail.com>
2023-05-12 05:26:30 +0600

    Update cms and design

--- src/components/organizations/AboutUsSection.jsx
+++ src/components/organizations/AboutUsSection.jsx
@@ -6,48 +6,55 @@ import ParagraphText from '../atoms/ParagraphText';
 import Heading2XL from '../atoms/Heading2XL';
 import Button from '../atoms/Button';
 import aboutBanner from '@/assets/images/aboutBanner.png';
-
+import { useTranslation } from 'react-i18next';
 
 const AboutUsSection = () => {
+  const {t,i18n}=useTranslation();
   return (
-    <div className='container-left grid  lg:grid-cols-2 gap-10 mx-auto ps-6 sm:ps-0 xl:ps-10  py-0 lg:py-3 mb-20 xl:mb-36'>
-      <div className='my-auto'>
-        <div className='space-y-6'>
-          <HeadingL>About Us</HeadingL>
+    <div className={`${i18n.language==='en' ? 'ltr:container-left ' : 'rtl:container-right'}`}>
+      <div className=' grid  lg:grid-cols-2 gap-10 mx-auto ltr:ps-6 ltr:sm:ps-0 ltr:xl:ps-10 rtl:pe-6 rtl:sm:pe-0 rtl:xl:pe-10  py-0 lg:py-3 mb-20 xl:mb-36'>
+        <div className='my-auto'>
+          <div className='space-y-6 max-w-2xl lg:max-w-4xl'>
+            <HeadingL>About Us</HeadingL>
 
-          <Heading5XL>
-            We Have 15 Years Of Experience On Business Development area
-          </Heading5XL>
-          <ParagraphText>
-            Masarat Digital is a Technology company that provide software
-            development, AI & Tech Consulting. Masarat Digital Company is
-            advance technology enabler in various industries/ tracks / paths
-            (HR, Healthcare, Supply Chain & Sports). For the consulting area,
-            Masarat is focusing on digital strategy, process reengineering,
-            digital marketing, digital transformation, digital enablement, and
-            advanced analytics.
-          </ParagraphText>
+            <Heading5XL>
+              We Have 15 Years Of Experience On Business Development area
+            </Heading5XL>
+            <ParagraphText>
+              Masarat Digital is a Technology company that provide software
+              development, AI & Tech Consulting. Masarat Digital Company is
+              advance technology enabler in various industries/ tracks / paths
+              (HR, Healthcare, Supply Chain & Sports). For the consulting area,
+              Masarat is focusing on digital strategy, process reengineering,
+              digital marketing, digital transformation, digital enablement, and
+              advanced analytics.
+            </ParagraphText>
+          </div>
+
+          <ul className='flex gap-10 mt-8'>
+            <li className='space-y-2'>
+              <Heading2XL>1432+</Heading2XL>
+              <ParagraphText>Happy Clients</ParagraphText>
+            </li>
+            <li className='space-y-2'>
+              <Heading2XL>1432+</Heading2XL>
+              <ParagraphText>Project Completed</ParagraphText>
+            </li>
+          </ul>
+
+          <Button
+            title='View Services'
+            className='bg-gradient-lightGreen text-slateBlue mt-10'
+          ></Button>
         </div>
-
-        <ul className='flex gap-10 mt-8'>
-          <li className='space-y-2'>
-            <Heading2XL>1432+</Heading2XL>
-            <ParagraphText>Happy Clients</ParagraphText>
-          </li>
-          <li className='space-y-2'>
-            <Heading2XL>1432+</Heading2XL>
-            <ParagraphText>Project Completed</ParagraphText>
-          </li>
-        </ul>
-
-        <Button
-          title='View Services'
-          className='bg-gradient-lightGreen text-slateBlue mt-10'
-        ></Button>
-      </div>
-      <div className='ms-auto'>
-        
-        <Image src={aboutBanner} alt=""/>
+        <div className='ltr:ms-auto rtl:me-auto'>
+          <Image
+            src={aboutBanner}
+            alt='about_cover'
+            className='rtl:-scale-x-100'
+            // className=' ltr:lg:right-0 rtl:lg:left-0 rtl:-scale-x-100  lg:absolute   w-full lg:w-auto h-full '
+          />
+        </div>
       </div>
     </div>
   );
