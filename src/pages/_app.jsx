83963c40e4a24222b975ee0c252313ffb0f9dca3
Shakil Ahmed <shakil@ourlegup.com>
2023-05-21 16:07:18 +0600

    Added favicon into the head

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,7 +3,7 @@ import i18n from '@/i18n/config'
 // import "~slick-carousel/slick/slick.css"; 
 // import "~slick-carousel/slick/slick-theme.css";
 
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
@@ -12,13 +12,13 @@ import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 
-export default function App({ Component, pageProps}) {
+export default function App({ Component, pageProps }) {
 
   const { i18n } = useTranslation();
 
-	useEffect(() => {
-		i18n.changeLanguage(localStorage.getItem('language'));
-	}, [i18n]);
+  useEffect(() => {
+    i18n.changeLanguage(localStorage.getItem('language'));
+  }, [i18n]);
 
   if (Component.getLayout) {
     return Component.getLayout(
